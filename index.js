3434cc392be2d1c4857bf10937032141573164d7
Chadi <chadi.toto@axa.fr>
2022-07-28 06:15:57 -0700

    removed module-alias for node subpaths

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-import Menu from "classes/menu";
+#!/usr/bin/env node
+
+import Menu from "#classes/menu";
 
 const app = new Menu();
 app.start();
--- middlewares/i18n.js
+++ middlewares/i18n.js
@@ -1,5 +1,8 @@
 import path from "path";
+import * as url from "url";
 import { I18n } from "i18n";
+
+const __dirname = url.fileURLToPath(new URL(".", import.meta.url));
 
 const i18n = new I18n({
   locales: ["en"],
